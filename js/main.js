27784cb87bef4bc777a8b7195774667cad31d3c4
danaSror <62503893+danaSror@users.noreply.github.com>
2020-05-07 20:38:40 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,27 @@
+var usersArr = [[ "p", "p"]];
+var player=[ "p", "p"];
+
+function clickPage(choosenPage) {
+    if(choosenPage=="welcome")
+    openPage("welcome");
+    if(choosenPage=="register")
+    openPage("register");
+    if(choosenPage=="login")
+    openPage("login");
+}
+
+function openPage(pageScriptName) {
+    if(pageScriptName != "game"  ){
+        _sound.pause();
+    }
+
+    //remove active class
+    const allSectionsActivated = document.getElementsByClassName("activeSection");
+    allSectionsActivated[0].classList.remove("activeSection");
+
+    //add active class
+    const newActivatedSection = document.getElementById("main").querySelector(`#${pageScriptName}Section`);
+    newActivatedSection.classList.add("activeSection");
+
+}
+
